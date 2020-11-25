85fc3cc6b9fec76c8f9ac3ec4cef1e1f09addc0d
Abraham <99671204@qq.com>
2020-11-25 14:04:59 +0800

    提示的封装

--- src/main.js
+++ src/main.js
@@ -48,6 +48,32 @@ uni.getSystemInfo({
 })
 
 
+const msg = (title, duration=1500, mask=false, icon='none')=>{
+	//统一提示方便全局修改
+	if(Boolean(title) === false){
+		return;
+	}
+	uni.showToast({
+		title,
+		duration,
+		mask,
+		icon
+	});
+}
+
+const prePage = ()=>{
+	let pages = getCurrentPages();
+	let prePage = pages[pages.length - 2];
+	// #ifdef H5
+	return prePage;
+	// #endif
+	return prePage.$vm;
+}
+
+Vue.prototype.$api = {msg, prePage};
+
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
