2af63071e176b8f149abb5328ee2a05ed6f3be46
Abraham <99671204@qq.com>
2020-12-24 15:05:38 +0800

    修改路由的导入1.5.5

--- src/main.js
+++ src/main.js
@@ -3,89 +3,86 @@ import App from './App'
 import MinRequest from './MinRequest'
 import minRequest from './api'
 import ABCache from './ABCache' // 缓存封装
-import {router,RouterMount} from './router.js'  //路径换成自己的
-import uView from "uview-ui";
+import { router, RouterMount } from './router.js' //路径换成自己的
+import uView from 'uview-ui'
 import cuCustom from './colorui/components/cu-custom.vue' // 自定义导航栏
 Vue.use(router)
-Vue.use(uView);
+Vue.use(uView)
 Vue.use(ABCache) // 缓存
-Vue.use(MinRequest)  // 请求
-
+Vue.use(MinRequest) // 请求
 
 Vue.component('cu-custom', cuCustom)
-let cache = new ABCache();
+let cache = new ABCache()
 uni.getSystemInfo({
 	success: function(e) {
 		// #ifndef MP
-		cache.set("_statusBar", e.statusBarHeight, 0)
+		cache.set('_statusBar', e.statusBarHeight, 0)
 		// Vue.prototype.StatusBar = e.statusBarHeight;
 		if (e.platform == 'android') {
-			cache.set("_customerBar", e.statusBarHeight + 50, 0)
+			cache.set('_customerBar', e.statusBarHeight + 50, 0)
 			// Vue.prototype.CustomBar = e.statusBarHeight + 50;
 		} else {
-			cache.set("_customerBar", e.statusBarHeight + 45, 0)
+			cache.set('_customerBar', e.statusBarHeight + 45, 0)
 			// Vue.prototype.CustomBar = e.statusBarHeight + 45;
-		};
+		}
 		// #endif
 
 		// #ifdef MP-WEIXIN
-		cache.set("_statusBar", e.statusBarHeight, 0)
+		cache.set('_statusBar', e.statusBarHeight, 0)
 		// Vue.prototype.StatusBar = e.statusBarHeight;
-		let custom = wx.getMenuButtonBoundingClientRect();
-		cache.set("_custom", custom, 0)
-		cache.set("_customerBar", custom.bottom + custom.top - e.statusBarHeight, 0)
+		let custom = wx.getMenuButtonBoundingClientRect()
+		cache.set('_custom', custom, 0)
+		cache.set('_customerBar', custom.bottom + custom.top - e.statusBarHeight, 0)
 		// Vue.prototype.Custom = custom;
 		// Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
-		// #endif		
+		// #endif
 
 		// #ifdef MP-ALIPAY
-		cache.set("_statusBar", e.statusBarHeight, 0)
+		cache.set('_statusBar', e.statusBarHeight, 0)
 		// Vue.prototype.StatusBar = e.statusBarHeight;
-		cache.set("_customerBar", e.statusBarHeight + e.titleBarHeight, 0)
+		cache.set('_customerBar', e.statusBarHeight + e.titleBarHeight, 0)
 		// Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
 		// #endif
 	}
 })
 
-
-const msg = (title, duration=1500, mask=false, icon='none')=>{
+const msg = (title, duration = 1500, mask = false, icon = 'none') => {
 	//统一提示方便全局修改
-	if(Boolean(title) === false){
-		return;
+	if (Boolean(title) === false) {
+		return
 	}
 	uni.showToast({
 		title,
 		duration,
 		mask,
 		icon
-	});
+	})
 }
 
-const prePage = ()=>{
-	let pages = getCurrentPages();
-	let prePage = pages[pages.length - 2];
+const prePage = () => {
+	let pages = getCurrentPages()
+	let prePage = pages[pages.length - 2]
 	// #ifdef H5
-	return prePage;
+	return prePage
 	// #endif
-	return prePage.$vm;
+	return prePage.$vm
 }
 
-Vue.prototype.$api = {msg, prePage};
-
-
+Vue.prototype.$api = { msg, prePage }
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+	...App,
+	minRequest
 })
 //v1.3.5起 H5端 你应该去除原有的app.$mount();使用路由自带的渲染方式
 // #ifdef H5
-	RouterMount(app,router,'#app')
+RouterMount(app, router, '#app')
 // #endif
 
 // #ifndef H5
-	app.$mount(); //为了兼容小程序及app端必须这样写才有效果
+app.$mount() //为了兼容小程序及app端必须这样写才有效果
 // #endif
