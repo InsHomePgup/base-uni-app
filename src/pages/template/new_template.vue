826a9921f84c85083ab87afdfc19abbfa5b465ad
Aka.braham <99671204@qq.com>
2022-01-07 09:57:41 +0800

    simple template

---
+++ src/pages/template/new_template.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<cu-custom bgColor="bg-blue" :isBack="true">
+			<block slot="content">导航栏名字</block>
+		</cu-custom>
+		<view class="container">
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad(){
+			this.loadData();
+		},
+		onReady(){
+			
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			async loadData(){
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container{
+		
+	}
+</style>
