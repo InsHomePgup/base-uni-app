08986eb22f57c4dc07265959ee08d2a51f746395
InsHomePgup <7328234@gmail.com>
2023-06-30 09:37:20 +0800

    add vconsole

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,58 @@
 <script>
-	export default {
-		onLaunch: function() {
-			// console.log('App Launch')
-		},
-		onShow: function() {
-			// console.log('App Show')
-		},
-		onHide: function() {
-			// console.log('App Hide')
-		}
-	}
+import VConsole from 'vconsole';
+
+const vConsole = new VConsole();
+// or init with options
+// const vConsole = new VConsole({ theme: 'dark' });
+
+// call `console` methods as usual
+console.log('Hello world');
+export default {
+    onLaunch: function () {
+
+
+        // console.log('App Launch')
+    },
+    onShow: function () {
+        // console.log('App Show')
+    },
+    onHide: function () {
+        // console.log('App Hide')
+    }
+}
 </script>
 
 <style lang="scss">
-	// 我的css库
-	@import "uview-ui/index.scss";
-	@import "colorui/main.css";
-	@import "colorui/icon.css";
-	/*每个页面公共css */
-	
-	view,
-	scroll-view,
-	swiper,
-	swiper-item,
-	cover-view,
-	cover-image,
-	icon,
-	text,
-	rich-text,
-	progress,
-	button,
-	checkbox,
-	form,
-	input,
-	label,
-	radio,
-	slider,
-	switch,
-	textarea,
-	navigator,
-	audio,
-	camera,
-	image,
-	video {
-		box-sizing: border-box;
-	}
-	
+// 我的css库
+@import "uview-ui/index.scss";
+@import "colorui/main.css";
+@import "colorui/icon.css";
+/*每个页面公共css */
+
+view,
+scroll-view,
+swiper,
+swiper-item,
+cover-view,
+cover-image,
+icon,
+text,
+rich-text,
+progress,
+button,
+checkbox,
+form,
+input,
+label,
+radio,
+slider,
+switch,
+textarea,
+navigator,
+audio,
+camera,
+image,
+video {
+    box-sizing: border-box;
+}
 </style>
